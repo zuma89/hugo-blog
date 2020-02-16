caaa25bd38f3259355debdaa51e08e8da87701b6
zuma89 <hilaryjhho@gmail.com>
2020-02-16 21:47:31 +0800

    Initial commit

---
+++ themes/ghostwriter/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const perfectionist = require('perfectionist');
+const discardComments = require('postcss-discard-comments');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    styles: path.join(__dirname, 'static', 'styles'),
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ],
+    extensions: ['.scss'],
+  },
+  output: {
+    path: path.join(__dirname, 'static', 'dist'),
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: discardComments,
+      canPrint: false
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: perfectionist,
+      cssProcessorOptions: {
+        format: 'compact'
+      },
+      canPrint: false
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      }
+    ]
+  }
+};
+
